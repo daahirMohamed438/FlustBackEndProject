ff3a687e94818d79d99112dfae4e92e48b1ca8bb
dahirmohamed <daahir@tabaarakict.org>
2025-09-03 19:52:27 +0300

    Added Firebase notification setup, booking create/cancel APIs, flat availability sorting, and status updates

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,15 +1,15 @@
 const admin = require("firebase-admin");
-require("dotenv").config(); // load .env variables
- 
-// Load the service account key JSON file
-const serviceAccount = require("./flusteruserandowner-firebase-adminsdk-fbsvc-f98e6d20b9.json");
+require("dotenv").config();
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://flusteruserandowner.firebaseio.com", // optional for Realtime DB
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+  databaseURL: process.env.FIREBASE_DB_URL,
 });
 
 module.exports = admin;
-
  
 
